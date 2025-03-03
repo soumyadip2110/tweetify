284504b4733097cfa5ec76b0de9e8f236029b9b0
soumyadip2110 <soumyadip2110@gmail.com>
2025-03-03 15:32:40 +0530

    add loading in login and signup page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,9 +7,10 @@ import { useNavigate } from 'react-router-dom'
 
 function Login() {
     const passInputRef = useRef(null)
-    
+
     const [email, setEmail] = useState('demo@example.com')
     const [password, setPassword] = useState('demo_password')
+    const [loading, setLoading] = useState(false)
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -17,11 +18,13 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
+            setLoading(true);
             const session = await authService.login({ 'email': email, 'password': password });
             if (session) {
                 const userData = await authService.getCurrentUser();
                 if (userData) {
                     dispatch(storeLogin(userData));
+                    setLoading(false);
                     navigate('/');
                 }
             } else {
@@ -33,63 +36,64 @@ function Login() {
     }
 
     const handlePassShow = () => {
-        if (passInputRef.current.type == 'password'){
+        if (passInputRef.current.type == 'password') {
             passInputRef.current.type = 'text'
         } else {
             passInputRef.current.type = 'password'
         }
     }
 
-    return (
-        <Container className='mt-[6rem] p-6'>
-            <h1 className='text-2xl font-bold'>Login to your account</h1>
-            <form className='w-full sm:w-1/2 md:w-1/3 m-auto mt-6 flex flex-col justify-center p-6 bg-[#687EFF] bg-opacity-10 border hover:shadow-[0px_0px_6px_#fff] duration-200 rounded-xl h-full' onSubmit={handleSubmit}>
-                <div className='p-2'>
-                    <Input
-                        label='Email:'
-                        labelText='white'
-                        placeholder='Enter your email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className='h-7'
-                        autoComplete="email"
-                    />
-                </div>
-                <div className='p-2'>
-                    <Input
-                        ref={passInputRef}
-                        label='Password:'
-                        type='password'
-                        labelText='white'
-                        placeholder='Enter your password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='h-7'
-                        autoComplete="current-password"
-                    />
-                    <Button
-                        type='button'
-                        className={`text-xs mt-1 ${password.length > 0 ? 'block' : 'hidden'}`}
-                        bgColor='bg-gray-500'
-                        py='1'
-                        px='2'
-                        onClick={handlePassShow}
-                    >
-                        Show
-                    </Button>
-                </div>
-                <div className='p-1'>
-                    <Button
-                        type='submit'
-                        className='mt-2 bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 hover:from-purple-500 hover:via-indigo-500 hover:to-blue-500 duration-200'
-                        bgColor='bg-[#475dde]'
-                    >
-                        Login
-                    </Button>
-                </div>
-            </form>
-        </Container>
-    )
+    return loading ? <h1 className='text-white mt-[2rem] md:mt-[1rem]'>Loading...</h1>
+        : (
+            <Container className='mt-[6rem] p-6'>
+                <h1 className='text-2xl font-bold'>Login to your account</h1>
+                <form className='w-full sm:w-1/2 md:w-1/3 m-auto mt-6 flex flex-col justify-center p-6 bg-[#687EFF] bg-opacity-10 border hover:shadow-[0px_0px_6px_#fff] duration-200 rounded-xl h-full' onSubmit={handleSubmit}>
+                    <div className='p-2'>
+                        <Input
+                            label='Email:'
+                            labelText='white'
+                            placeholder='Enter your email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='h-7'
+                            autoComplete="email"
+                        />
+                    </div>
+                    <div className='p-2'>
+                        <Input
+                            ref={passInputRef}
+                            label='Password:'
+                            type='password'
+                            labelText='white'
+                            placeholder='Enter your password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className='h-7'
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type='button'
+                            className={`text-xs mt-1 ${password.length > 0 ? 'block' : 'hidden'}`}
+                            bgColor='bg-gray-500'
+                            py='1'
+                            px='2'
+                            onClick={handlePassShow}
+                        >
+                            Show
+                        </Button>
+                    </div>
+                    <div className='p-1'>
+                        <Button
+                            type='submit'
+                            className='mt-2 bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 hover:from-purple-500 hover:via-indigo-500 hover:to-blue-500 duration-200'
+                            bgColor='bg-[#475dde]'
+                        >
+                            Login
+                        </Button>
+                    </div>
+                </form>
+            </Container>
+        )
 }
 
 export default Login
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,19 +7,20 @@ import { useNavigate } from 'react-router-dom'
 
 function Signup() {
     const passInputRef = useRef(null)
-    
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [username, setUsername] = useState('')
+    const [loading, setLoading] = useState(false)
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    
+
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        setLoading(true);
         // Check if email is valid
         if (emailRegex.test(email)) {
             try {
@@ -28,6 +29,7 @@ function Signup() {
                     const userData = await authService.getCurrentUser();
                     if (userData) {
                         dispatch(storeLogin(userData));
+                        setLoading(false);
                         navigate('/');
                     }
                 } else {
@@ -37,81 +39,82 @@ function Signup() {
                 console.log(error);
             }
         }
-        else{
+        else {
             alert('Please enter a valid email!')
         }
     }
 
     const handlePassShow = () => {
-        if (passInputRef.current.type == 'password'){
+        if (passInputRef.current.type == 'password') {
             passInputRef.current.type = 'text'
         } else {
             passInputRef.current.type = 'password'
         }
     }
 
-    return (
-        <Container className='mt-[6rem] p-6'>
-            <h1 className='text-2xl font-bold'>Signup and create a account</h1>
-            <form className='w-full sm:w-1/2 md:w-1/3 m-auto mt-6 flex flex-col justify-center p-6 bg-[#687EFF] bg-opacity-10 border hover:shadow-[0px_0px_6px_#fff] duration-200 rounded-xl h-full' onSubmit={handleSubmit}>
-                <div className='p-2'>
-                    <Input
-                        label='Username:'
-                        labelText='white'
-                        placeholder='Enter a username'
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        className='h-7'
-                        autoComplete="username"
-                    />
-                </div>
-                <div className='p-2'>
-                    <Input
-                        label='Email:'
-                        labelText='white'
-                        placeholder='Enter your email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className='h-7'
-                        autoComplete="email"
-                    />
-                </div>
-                <div className='p-2'>
-                    <Input
-                        ref={passInputRef}
-                        label='Password:'
-                        labelText='white'
-                        type='password'
-                        placeholder='Enter your password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='h-7'
-                        autoComplete="new-password"
-                    />
-                    <Button
-                        type='button'
-                        className={`text-xs mt-1 ${password.length > 0 ? 'block' : 'hidden'}`}
-                        bgColor='bg-gray-500'
-                        py='1'
-                        px='2'
-                        onClick={handlePassShow}
-                    >
-                        Show
-                    </Button>
-                </div>
-                <div className='p-2'>
-                    <Button
-                        type='submit'
-                        className='mt-2  bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 hover:from-purple-500 hover:via-indigo-500 hover:to-blue-500 duration-200'
-                        bgColor='bg-[#475dde]'
-                    >
-                        Signup
-                    </Button>
-                </div>
-            </form>
-            <h1 className=''>Password must be atleast 8 characters long</h1>
-        </Container>
-    )
+    return loading ? <h1 className='text-white mt-[2rem] md:mt-[1rem]'>Loading...</h1>
+        : (
+            <Container className='mt-[6rem] p-6'>
+                <h1 className='text-2xl font-bold'>Signup and create a account</h1>
+                <form className='w-full sm:w-1/2 md:w-1/3 m-auto mt-6 flex flex-col justify-center p-6 bg-[#687EFF] bg-opacity-10 border hover:shadow-[0px_0px_6px_#fff] duration-200 rounded-xl h-full' onSubmit={handleSubmit}>
+                    <div className='p-2'>
+                        <Input
+                            label='Username:'
+                            labelText='white'
+                            placeholder='Enter a username'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            className='h-7'
+                            autoComplete="username"
+                        />
+                    </div>
+                    <div className='p-2'>
+                        <Input
+                            label='Email:'
+                            labelText='white'
+                            placeholder='Enter your email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='h-7'
+                            autoComplete="email"
+                        />
+                    </div>
+                    <div className='p-2'>
+                        <Input
+                            ref={passInputRef}
+                            label='Password:'
+                            labelText='white'
+                            type='password'
+                            placeholder='Enter your password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className='h-7'
+                            autoComplete="new-password"
+                        />
+                        <Button
+                            type='button'
+                            className={`text-xs mt-1 ${password.length > 0 ? 'block' : 'hidden'}`}
+                            bgColor='bg-gray-500'
+                            py='1'
+                            px='2'
+                            onClick={handlePassShow}
+                        >
+                            Show
+                        </Button>
+                    </div>
+                    <div className='p-2'>
+                        <Button
+                            type='submit'
+                            className='mt-2  bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 hover:from-purple-500 hover:via-indigo-500 hover:to-blue-500 duration-200'
+                            bgColor='bg-[#475dde]'
+                        >
+                            Signup
+                        </Button>
+                    </div>
+                </form>
+                <h1 className=''>Password must be atleast 8 characters long</h1>
+            </Container>
+        )
 }
 
 export default Signup
